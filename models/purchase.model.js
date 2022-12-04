97def5b7b93c429d2c0eb59a88abc18826298933
Rosana <rcathayde2012@gmail.com>
2022-12-04 14:42:08 -0300

    Segundo commit: Alteração Purchase Model

---
+++ models/purchase.model.js
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose"
+
+const purchaseSchema = new Schema(
+{
+shippingAdress: {
+    type: String
+},
+album: [
+{
+   type: Schema.Types.ObjectId,
+   ref: "Album"
+}
+]
+},
+{
+    timestamps: true,
+}
+)
+const PurchaseModel = model("Purchase",purchaseSchema)
+export default PurchaseModel
